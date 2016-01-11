0217e4c6f7c8b89c37cc25055d22b5500ba0141f
LuckyJayce <Administrator@QH-20150506888>
2016-01-11 10:17:44 +0800

    VaryViewHelper添加一个构造方法 public VaryViewHelper(View view, LayoutParams selfParams)

--- MVCHelper_Library/src/com/shizhefei/view/vary/VaryViewHelper.java
+++ MVCHelper_Library/src/com/shizhefei/view/vary/VaryViewHelper.java
@@ -16,6 +16,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 
 /**
  * 用于切换布局,用一个新的布局替换掉原先的布局
@@ -24,32 +25,45 @@ import android.view.ViewGroup;
  *
  */
 public class VaryViewHelper implements IVaryViewHelper {
-	private View view;
+	private View originalView;
 	private ViewGroup parentView;
 	private int viewIndex;
-	private ViewGroup.LayoutParams params;
+	private LayoutParams originalParams;
+	private LayoutParams selfParams;
 	private View currentView;
 
 	public VaryViewHelper(View view) {
 		super();
-		this.view = view;
+		this.originalView = view;
+	}
+
+	/**
+	 * 
+	 * @param view
+	 * @param selfParams
+	 *            替换view的时候用selfParams设置，originalView依然使用originalParams设置
+	 */
+	public VaryViewHelper(View view, LayoutParams selfParams) {
+		super();
+		this.originalView = view;
+		this.selfParams = selfParams;
 	}
 
 	private void init() {
-		params = view.getLayoutParams();
-		if (view.getParent() != null) {
-			parentView = (ViewGroup) view.getParent();
+		originalParams = originalView.getLayoutParams();
+		if (originalView.getParent() != null) {
+			parentView = (ViewGroup) originalView.getParent();
 		} else {
-			parentView = (ViewGroup) view.getRootView().findViewById(android.R.id.content);
+			parentView = (ViewGroup) originalView.getRootView().findViewById(android.R.id.content);
 		}
 		int count = parentView.getChildCount();
 		for (int index = 0; index < count; index++) {
-			if (view == parentView.getChildAt(index)) {
+			if (originalView == parentView.getChildAt(index)) {
 				viewIndex = index;
 				break;
 			}
 		}
-		currentView = view;
+		currentView = originalView;
 	}
 
 	@Override
@@ -59,7 +73,7 @@ public class VaryViewHelper implements IVaryViewHelper {
 
 	@Override
 	public void restoreView() {
-		showLayout(view);
+		showLayout(originalView);
 	}
 
 	@Override
@@ -75,7 +89,11 @@ public class VaryViewHelper implements IVaryViewHelper {
 				parent.removeView(view);
 			}
 			parentView.removeViewAt(viewIndex);
-			parentView.addView(view, viewIndex, params);
+			if (view != originalView && selfParams != null) {
+				parentView.addView(view, viewIndex, selfParams);
+			} else {
+				parentView.addView(view, viewIndex, originalParams);
+			}
 		}
 	}
 
@@ -86,16 +104,16 @@ public class VaryViewHelper implements IVaryViewHelper {
 
 	@Override
 	public View inflate(int layoutId) {
-		return LayoutInflater.from(view.getContext()).inflate(layoutId, null);
+		return LayoutInflater.from(originalView.getContext()).inflate(layoutId, null);
 	}
 
 	@Override
 	public Context getContext() {
-		return view.getContext();
+		return originalView.getContext();
 	}
 
 	@Override
 	public View getView() {
-		return view;
+		return originalView;
 	}
 }
