0bf52257ba47ea6b54ddd25f53f2caccd6f442a5
zsy <794629068@qq.com>
2016-05-26 18:39:10 +0800

    更新界面的 item

--- mvchelper_library/src/main/java/com/shizhefei/mvc/RequestHandle.java
+++ mvchelper_library/src/main/java/com/shizhefei/mvc/RequestHandle.java
@@ -1,9 +1,12 @@
 package com.shizhefei.mvc;
 
+/**
+ * 用于外部取消请求的处理。
+ */
 public interface RequestHandle {
 
-	public void cancle();
+    public void cancle();
 
-	public boolean isRunning();
+    public boolean isRunning();
 
 }
--- mvchelper_library/src/main/java/com/shizhefei/mvc/ResponseSender.java
+++ mvchelper_library/src/main/java/com/shizhefei/mvc/ResponseSender.java
@@ -1,5 +1,9 @@
 package com.shizhefei.mvc;
 
+/**
+ * 用于请求结束时发送数据或者发送异常
+ * @param <DATA>
+ */
 public interface ResponseSender<DATA> {
 
 	public void sendError(Exception exception);
