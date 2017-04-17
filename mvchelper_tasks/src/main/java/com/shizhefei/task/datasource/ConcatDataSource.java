83f5540bde81f0a5ee0f0ac3292ad0964ac0f197
zsy <shengyou.zhang@enai.im>
2017-04-18 01:23:17 +0800

    添加注释

--- mvchelper_tasks/src/main/java/com/shizhefei/task/datasource/ConcatDataSource.java
+++ mvchelper_tasks/src/main/java/com/shizhefei/task/datasource/ConcatDataSource.java
@@ -14,7 +14,7 @@ import com.shizhefei.task.tasks.Tasks;
  * Created by luckyjayce on 2017/4/17.
  */
 
-public class ConcatDataSource<D, DATA> implements IAsyncDataSource<DATA> {
+class ConcatDataSource<D, DATA> implements IAsyncDataSource<DATA> {
     private IAsyncDataSource<DATA> dataSource;
     private IAsyncTask<D> asyncTask;
     private ResponseSenderCallback<DATA> successCallback;
