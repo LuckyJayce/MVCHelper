4d72ac3f940185f43acc44babc8a2ba173f38209
zsy <shengyou.zhang@enai.im>
2017-04-18 01:21:09 +0800

    添加注释

--- mvchelper_tasks/src/main/java/com/shizhefei/task/tasks/Tasks.java
+++ mvchelper_tasks/src/main/java/com/shizhefei/task/tasks/Tasks.java
@@ -14,6 +14,12 @@ import com.shizhefei.task.function.Func2;
 
 public class Tasks {
 
+    /**
+     * 将一个同步task转化为异步task
+     * @param task
+     * @param <DATA>
+     * @return
+     */
     public static <DATA> LinkTask<DATA> async(ITask<DATA> task) {
         return new AsyncLinkTask<>(task, true);
     }
@@ -22,10 +28,22 @@ public class Tasks {
         return new DataLinkTask<>(data);
     }
 
+    /**
+     * 通过同步task创建一个链式task
+     * @param task
+     * @param <DATA>
+     * @return 一个链式task
+     */
     public static <DATA> LinkTask<DATA> create(ITask<DATA> task) {
         return new AsyncLinkTask<>(task, true);
     }
 
+    /**
+     * 通过异步task创建一个链式task
+     * @param task
+     * @param <DATA>
+     * @return 一个链式task
+     */
     public static <DATA> LinkTask<DATA> create(IAsyncTask<DATA> task) {
         if (task instanceof LinkTask) {
             return (LinkTask<DATA>) task;
@@ -33,20 +51,42 @@ public class Tasks {
         return new LinkProxyTask<>(task);
     }
 
+    /**
+     * 通过一个同步dataSource创建一个链式task
+     * @param dataSource
+     * @param isExeRefresh true：dataSource的refresh转成task，false：dataSource的loadMore转成task
+     * @param <DATA>
+     * @return 一个链式task
+     */
     public static <DATA> LinkTask<DATA> create(IDataSource<DATA> dataSource, boolean isExeRefresh) {
         return new AsyncLinkTask<>(dataSource, isExeRefresh);
     }
 
+    /**
+     * 通过一个异步dataSource创建一个链式task
+     * @param dataSource
+     * @param isExeRefresh true：dataSource的refresh转成task，false：dataSource的loadMore转成task
+     * @param <DATA>
+     * @return 一个链式task
+     */
     public static <DATA> LinkTask<DATA> create(IAsyncDataSource<DATA> dataSource, boolean isExeRefresh) {
         return new AsyncLinkTask<>(dataSource, isExeRefresh);
     }
 
+    /**
+     * 按两个task先后顺序执行，task1的结果可以作为task的参数
+     * @param task task1
+     * @param func 将task的结果传给task2，返回一个可执行task2
+     * @param <D> 数据1
+     * @param <DATA> 结果数据
+     * @return
+     */
     public static <D, DATA> LinkTask<DATA> concatMap(IAsyncTask<D> task, Func1<D, IAsyncTask<DATA>> func) {
         return new ConcatLinkTask<>(task, func);
     }
 
     /**
-     * 连接两个task
+     * 按两个task先后顺序执行
      *
      * @param task
      * @param task2
@@ -63,6 +103,16 @@ public class Tasks {
         });
     }
 
+    /**
+     * 合并两个task，两个task一起执行，其中一个报错就停止执行，func将两个d1和d2的结果转化成最终的data
+     * @param task1
+     * @param task2
+     * @param func
+     * @param <D1>
+     * @param <D2>
+     * @param <DATA>
+     * @return
+     */
     public static <D1, D2, DATA> LinkTask<DATA> combine(IAsyncTask<D1> task1, IAsyncTask<D2> task2, Func2<D1, D2, DATA> func) {
         return new CombineTask<>(task1, task2, func);
     }
