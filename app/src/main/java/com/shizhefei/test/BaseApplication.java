4ca1286c45eca128b410637f559a02bb9dae5d2f
LuckyJayce <794629068@qq.com>
2018-11-04 03:54:07 +0800

    删除未用到的代码

--- app/src/main/java/com/shizhefei/test/BaseApplication.java
+++ app/src/main/java/com/shizhefei/test/BaseApplication.java
@@ -4,14 +4,12 @@ import android.app.Application;
 
 import com.shizhefei.mvc.MVCHelper;
 import com.shizhefei.test.view.MyLoadViewFactory;
-import com.shizhefei.utils.MyVolley;
 import com.squareup.leakcanary.LeakCanary;
 
 public class BaseApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		MyVolley.init(getApplicationContext());
 		
 		// 设置LoadView的factory，用于创建使用者自定义的加载失败，加载中，加载更多等布局,写法参照DeFaultLoadViewFactory
 		MVCHelper.setLoadViewFractory(new MyLoadViewFactory());
